8191b73bbca490777263a168eaeec16b7ab3b5eb
carlosnsilva <cmanoel17@gmail.com>
2024-06-12 11:37:13 -0300

    Add config for Cucumber

--- src/test/java/tests/config/cucumberTests.java
+++ src/test/java/tests/config/cucumberTests.java
@@ -1,4 +1,10 @@
 package tests.config;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features")
 public class cucumberTests {
 }
